c28187c07ed1359c314cd2e3387abc103ec2e123
Milan Meulemans <milan.meulemans@live.be>
2025-06-23 23:50:09 +0200

    Add password protection

--- main.py
+++ main.py
@@ -51,38 +51,37 @@ with col_h1:
 
 
 def check_password():
-    # """Returns `True` if the user had the correct password."""
-    #
-    # def password_entered():
-    #     """Checks whether a password entered by the user is correct."""
-    #     if st.session_state["password"] == st.secrets["password"]:
-    #         st.session_state["password_correct"] = True
-    #         del st.session_state["password"]  # don't store password
-    #     else:
-    #         st.session_state["password_correct"] = False
-    #
-    # if "password_correct" not in st.session_state:
-    #     # First run, show input for password.
-    #     col_pass1, col_pass2, col_pass3 = st.columns([1,3,1])
-    #     with col_pass2:
-    #         st.text_input(
-    #             "Password", type="password", on_change=password_entered, key="password"
-    #         )
-    #     return False
-    # elif not st.session_state["password_correct"]:
-    #     # Password not correct, show input + error.
-    #     col_pass1, col_pass2, col_pass3 = st.columns([1,3,1])
-    #     with col_pass2:
-    #         st.text_input(
-    #             "Password", type="password", on_change=password_entered, key="password"
-    #         )
-    #     col_passerr1, col_passerr2, col_passerr3 = st.columns([1,3,1])
-    #     with col_passerr2:
-    #         st.error("😕 Password incorrect")
-    #     return False
-    # else:
-    #     # Password correct.
-    return True
+    """Returns `True` if the user had the correct password."""
+    
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+    
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        col_pass1, col_pass2, col_pass3 = st.columns([1,3,1])
+        with col_pass2:
+            st.text_input(
+                "Password", type="password", on_change=password_entered, key="password"
+            )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        col_pass1, col_pass2, col_pass3 = st.columns([1,3,1])
+        with col_pass2:
+            st.text_input(
+                "Password", type="password", on_change=password_entered, key="password"
+            )
+        col_passerr1, col_passerr2, col_passerr3 = st.columns([1,3,1])
+        with col_passerr2:
+            st.error("😕 Password incorrect")
+        return False
+    else:
+        return True
 
 if check_password():
 
